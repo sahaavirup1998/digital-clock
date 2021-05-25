58e306f6d47c7bf22f96e8f1ee7a895b215b4967
Avirup Saha <sahaavirup1998@gmail.com>
2021-05-25 22:28:45 +0530

    idgital-clock

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component, useState } from "react";
+//import "../styles/App.css"; 
+
+class App extends Component {
+
+  constructor(props) {
+    super();
+    this.state = { time: new Date() };
+  }
+
+  componentDidMount() {
+    setInterval(this.currentTime, 1000);
+  }
+
+  componentDidUpdate() {
+    this.interval = setInterval(this.currentTime, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  currentTime = () => {
+    this.setState({time:new Date()})
+  }
+
+  render() {
+    return (
+      <>
+        <div className="Clock">
+          <h3 id="time">{this.state.time.toLocaleTimeString()}</h3>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
